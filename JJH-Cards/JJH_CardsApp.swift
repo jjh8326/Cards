73d1d651023a0576ca1204cbadd43bb77f5d0310
Joe Harasz <j30h30@github.com>
2024-09-07 18:20:27 -0400

    Initial Commit

---
+++ JJH-Cards/JJH_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJH_CardsApp.swift
+//  JJH-Cards
+//
+//  Created by Joe H on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct JJH_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
